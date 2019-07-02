c43af73c47d8535f2960995eb8c2f3ff6c5cba52
TechnicallyRachel <paintmetothemoon@gmail.com>
2019-07-02 13:58:04 -0700

    Updates

---
+++ precache-manifest.83d429c48d72b3051ce988bc040b2335.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4830b341a5eae9b6e57e479cf1f9888f",
+    "url": "/firstyearhub/index.html"
+  },
+  {
+    "revision": "940b534e2f4d6f4de582",
+    "url": "/firstyearhub/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/firstyearhub/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "940b534e2f4d6f4de582",
+    "url": "/firstyearhub/static/js/main.6d3bfcdb.chunk.js"
+  },
+  {
+    "revision": "a11223770fc52566bfb6",
+    "url": "/firstyearhub/static/js/runtime~main.71b19294.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/firstyearhub/static/media/logo.5d5d9eef.svg"
+  }
+]);
